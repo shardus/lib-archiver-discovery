043b35426dbe2d1d379fe4dd3a56adfd1d220ee5
Arham Jain <arham.nec@gmail.com>
2023-05-16 11:37:47 +0530

    fix(ut): reduced timeout in test to run faster in gitlab pipeline

--- test/src/index.test.ts
+++ test/src/index.test.ts
@@ -28,6 +28,7 @@ test('test getArchiverList with a valid env, valid config and seed list url', as
   try {
     await setupArchiverDiscovery({
       hashKey: '69fa4195670576c0160d660c3be36556ff8d504725be8a59b5a96509e0c994bc',
+      archiverTimeoutInMilliSeconds: 100,
     })
   } catch (err) {
     console.log(err.message)
